d16285ebbca1d8177dcae0687e97646cfc4877f2
Andres Florez <andres.florez03@uptc.edu.co>
2018-04-12 17:30:26 -0500

    Realizada estructura del nodo- letra

--- src/models/NodeLetter.java
+++ src/models/NodeLetter.java
@@ -1,5 +1,27 @@
 package models;
 
 public class NodeLetter {
+	private char info;
+	private int mark;
+
+	public NodeLetter(char info) {
+		this.info = info;
+	}
+
+	public char getInfo() {
+		return info;
+	}
+
+	public void setInfo(char info) {
+		this.info = info;
+	}
+
+	public int getMark() {
+		return mark;
+	}
+
+	public void setMark(int mark) {
+		this.mark = mark;
+	}
 
 }
