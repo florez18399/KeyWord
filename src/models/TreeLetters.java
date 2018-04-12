1f6ffc7f78e6285b730f1edce396f5019dac8fa4
Andres Florez <andres.florez03@uptc.edu.co>
2018-04-12 17:57:29 -0500

    Realizado método de agregación de palabra

--- src/models/TreeLetters.java
+++ src/models/TreeLetters.java
@@ -7,6 +7,35 @@ public class TreeLetters {
 		root = new NodeLetter('R');
 	}
 
+	/**
+	 * Agregar al árbol
+	 */
+	public void addWord(String word) {
+		addWord(0, word, root);
+	}
+
+	private void addWord(int i, String word, NodeLetter actualLetter) {
+		NodeLetter foundLetter = searchChildRoot(actualLetter, word.charAt(i));
+		if (foundLetter != null) {
+			addWord(i + 1, word, foundLetter);
+		} else {
+			NodeLetter nodeFather = actualLetter;
+			while (i < word.length()) {
+				nodeFather.getChildren().add(new NodeLetter(word.charAt(i)));
+				nodeFather = nodeFather.getChildren().get(0);
+				i++;
+			}
+		}
+	}
+
+	private NodeLetter searchChildRoot(NodeLetter father, char letter) {
+		for (NodeLetter nodeLetter : father.getChildren()) {
+			if (nodeLetter.getInfo() == letter)
+				return nodeLetter;
+		}
+		return null;
+	}
+
 	public NodeLetter getRoot() {
 		return root;
 	}
